132f52e6c203a4a23beba97abdd56a49a1bf5ae8
AranzaGtz <aranza.gutierrezm.ok@gmail.com>
2022-11-17 21:13:59 -0800

    Update main.py
    Métodos agregados

--- programs/main.py
+++ programs/main.py
@@ -2,13 +2,30 @@
 import os
 import sys
 import getpass
+from numpy import array
+from numpy import* 
 #--------------------------- ASIGNACIONES -----------------------
-password = ( '123' )
-user = ('Moy')
+v_password = ( '123' , 'hola' , 'Soy1humano?')
+v_user = ('moy' , 'aranza' , 'miguel')
 #---------------------------- FUNCIONES ------------------------
-def acceso(p,u):
+# METODO DE ACCESO TIPO LOGIN
+def acceso():
     
     while True:
         os.system ("clear")#LIMPIAR PANTALLA
         print("\n\tInicio de secion")
-        u=input()
+        user = str(input("\n\t\tUsuario: "))
+        if user in v_user :
+            password = getpass.getpass("\n\tContraseña: ")
+            if password in v_password :
+                print(f"\n\tBienvenido usuario {user} ...")
+                break
+            else:
+                input("\n\tcontraseña incorrecta.\n\tEnter para continuar...")
+        else:
+            input("\n\tUsuario incorrecto.\n\tEnter para continuar...")
+    input("\n\tEnter para continuar...")
+
+# METODO DE MENU
+def menu():
+    
